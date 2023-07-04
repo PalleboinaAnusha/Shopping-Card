78ad53694eff553297b0d8db13f6a03e488a6ad5
Anusha Palleboina <apalleboina@innominds.com>
2023-07-04 11:29:48 +0530

    reactshoping cart

---
+++ ecommerce/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css';
+function Header(props) {
+  return (
+    <div className='flex shopping-card'>
+      <div onClick={() => props.handleShow(false)}>Shopping Card App</div>
+      <div onClick={() => props.handleShow(true)}>cart
+        <sup>{props.count}</sup>
+      </div>
+    </div>
+  )
+}
+
+export default Header
